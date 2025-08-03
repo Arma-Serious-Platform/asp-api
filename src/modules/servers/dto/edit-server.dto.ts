f3244163d8835be860014ed4154e8f799bf5d008
AgentosUA <agentoswork@gmail.com>
2025-08-03 13:03:46 +0300

    chore: update swagger docs

--- src/modules/servers/dto/edit-server.dto.ts
+++ src/modules/servers/dto/edit-server.dto.ts
@@ -1,16 +1,20 @@
 import { Optional } from "@nestjs/common";
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { ServerStatus } from "@prisma/client";
 import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class EditServerDto {
+  @ApiPropertyOptional()
   @IsString()
   @Optional()
   name?: string;
 
+  @ApiPropertyOptional()
   @IsString()
   @Optional()
   ip?: string;
 
+  @ApiPropertyOptional()
   @IsNumber()
   @Optional()
   port?: number;
