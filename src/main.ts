9a84b0ca3de9039dfecad0fc62d202f19d5d205e
Oleh Stepaniuk <oleh.stepaniuk@chatbots.studio>
2025-09-03 23:01:46 +0300

    feat: add dark theme to swagger

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,38 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { seed } from 'prisma/seed';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
   await seed();
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    skipMissingProperties: true,
-    skipUndefinedProperties: false,
-    whitelist: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      skipMissingProperties: true,
+      skipUndefinedProperties: false,
+      whitelist: true,
+    }),
+  );
 
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-  })
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Arma Serious Platform API')
     .setDescription('The core ASP API service')
     .setVersion(process.env.npm_package_version ?? '0.0.1')
     .build();
+  const theme = new SwaggerTheme();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+    explorer: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
