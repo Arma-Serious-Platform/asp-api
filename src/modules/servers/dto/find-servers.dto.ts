8871d4ed3c651fda102ca6a8459e51d0e3b3305e
AgentosUA <agentoswork@gmail.com>
2026-01-10 17:35:14 +0200

    fix: displaying servers

--- src/modules/servers/dto/find-servers.dto.ts
+++ src/modules/servers/dto/find-servers.dto.ts
@@ -1,18 +1,17 @@
-import { Optional } from "@nestjs/common";
 import { ApiPropertyOptional } from "@nestjs/swagger";
 import { ServerStatus } from "@prisma/client";
 import { Transform } from "class-transformer";
-import { IsBoolean, IsEnum } from "class-validator";
+import { IsBoolean, IsEnum, IsOptional } from "class-validator";
 import { PaginationDto } from "src/shared/dto/pagination.dto";
 
 export class FindServersDto extends PaginationDto {
   @ApiPropertyOptional()
-  @Optional()
+  @IsOptional()
   @IsEnum(ServerStatus)
   status?: ServerStatus;
 
   @ApiPropertyOptional()
-  @Optional()
+  @IsOptional()
   @Transform(({ value }) => value === 'true')
   @IsBoolean()
   fetchActualInfo?: boolean;
